610422e2777a7e5e4dc15cce0e6886b7e6d31872
ADRIJA ROY <anshuroy11012007@gmail.com>
2026-05-03 10:30:56 +0530

    Add files via upload

---
+++ bt inorder.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+    struct node*l;
+    int data;
+    struct node* r;
+};
+void inOrder(struct node* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    inOrder(root->l);           // Visit Left
+    printf("%d ", root->data);  // Visit Root
+    inOrder(root->r);           // Visit Right
+}
+int main(){
+    struct node* a = (struct node*)malloc(sizeof(struct node));
+    struct node* b = (struct node*)malloc(sizeof(struct node));
+    struct node* c = (struct node*)malloc(sizeof(struct node));
+    struct node* d = (struct node*)malloc(sizeof(struct node));
+    struct node* e = (struct node*)malloc(sizeof(struct node));
+    struct node* f = (struct node*)malloc(sizeof(struct node));
+    struct node* g = (struct node*)malloc(sizeof(struct node));
+    struct node* head = a;
+    a -> l = b;
+    a -> data = 90;
+    a -> r = c;
+    b -> l = d;
+    b -> data = 40;
+    b -> r = e;
+    c -> l = f;
+    c -> data = 70;
+    c -> r = g;
+    e -> l = NULL;
+    e -> data = 10;
+    e -> r = NULL;
+    f -> l = NULL;
+    f -> data = 50;
+    f -> r = NULL;
+    d -> l = NULL;
+    d -> data = 30;
+    g -> l = NULL;
+    g -> data = 65;
+    d -> r = NULL;
+    g -> r = NULL;
+    printf("In-order Traversal: ");
+    inOrder(head);
+    return 0;
+}
